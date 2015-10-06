846c56f408d4287647318e698413905f4bbc9236
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2015-10-06 22:06:52 +0900

    Rename method

--- Tests/Classes/CocoaLumberjackTests.m
+++ Tests/Classes/CocoaLumberjackTests.m
@@ -10,17 +10,7 @@ static const DDLogLevel ddLogLevel = DDLogLevelWarning;
 
 @implementation CocoaLumberjackTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
+- (void)testLogger {
     id<DDLogger> consoleLogger = [DDTTYLogger sharedInstance];
 
     consoleLogger.logFormatter = [[K9LogLumberjackPatternFormatter alloc] initWithPattern:@"%.1p: %m"];
